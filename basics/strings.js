fd94976a9302426843deffea71e902617030bbe2
Gabriel Balog <gabriel.balog@bradootech.com>
2018-08-31 20:04:27 -0300

    2. Strings and Variables

---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+let firstName = 'Gabriel'
+let lastName = 'Balog'
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+let city = "São Paulo"
+let country = "Brasil"
+let location = city + ", " + country
+
+console.log(location)
