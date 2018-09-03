898b2f5569ce329aacab3ef1e605abb1a9cf7546
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-02 21:19:32 -0300

    3 - 4. More on Variables

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+let petName = 'Sniff'
+// let petname = 'Nymeria'
+petname = 'Nymeria'
+
+console.log(petName)
+
+// Nao pode-se iniciar uma variavel com um numero, somente com letras
+// Nao se pode usar simbolos, somente "- e "_"
+// let 3 = 2
+// let result = 3 + 2
+
+// Nao se pode nomear uma variavel com uma palavra reservada
+// let let = 12
