0f551482e1fa33d05f06d95d9c391ed8a75c9d09
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-02 22:05:19 -0300

    3 - 6. Booleans and Comparison Operators

---
+++ basics/boolean.js
@@ -0,0 +1,23 @@
+// === - equality operator
+// !== not equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less or equal to operator
+// >= - greater or equal to operator
+
+// let temp = 31
+// let isFreezing = temp === 31 // -> true
+// let isFreezing = temp !== 31 // -> false
+// let isFreezing = temp < 31 // -> false
+// let isFreezing = temp <= 31 // -> true
+// let isFreezing = temp > 31 // -> false
+// let isFreezing = temp >= 31 // -> true
+
+// console.log(isFreezing)
+
+let age = 21
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
