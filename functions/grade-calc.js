2aed0a2089e2e75400042e58cf895ca30e90a2ef
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-03 12:58:44 -0300

    4 - 7. Build a Grade Calculator

---
+++ functions/grade-calc.js
@@ -0,0 +1,24 @@
+let gradeCalc = function(studentScore, possibleScore) {
+    score = (studentScore / possibleScore) * 100
+    let letterGrade = ''
+
+    if (score >= 90) {
+        letterGrade = 'A'
+    } else if (score >= 80) {
+        letterGrade = 'B'
+    } else if (score >= 70) {
+        letterGrade = 'C'
+    } else if (score >= 60) {
+        letterGrade = 'D'
+    } else {
+        letterGrade = 'F'
+    }
+    
+    return `You gote a ${letterGrade} (${score}%)!`
+}
+
+gradeOne = gradeCalc(15, 20)
+gradeTwo = gradeCalc(5, 20)
+
+console.log(gradeOne)
+console.log(gradeTwo)
