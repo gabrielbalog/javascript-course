e9b1fea2b8b4edb10a20dba5e42a9a75a4e35afb
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-02 21:15:03 -0300

    3 - 3. Numbers

---
+++ basics/numbers.js
@@ -0,0 +1,19 @@
+// let x = 11
+// let num = 5.5 + 2
+// let num = 5.5 - 2 
+// let num = 5.5 * 2 
+// let num = 5.5 / 2
+// let num = x + 1 * 2
+
+// console.log(num)
+
+// let age = 21
+// let dogYears = (age + 1) / 7
+
+// console.log(dogYears)
+
+studentScore = 18
+maxScore = 20
+percent = (studentScore / maxScore) * 100
+
+console.log(percent)
