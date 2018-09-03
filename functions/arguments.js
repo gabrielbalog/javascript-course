fcebeb2f723f3849f5f1b85d448db23d9ecc95ee
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-03 12:46:31 -0300

    4 - 6. Template Strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -8,9 +8,7 @@ console.log(result) // 16
 
 // Default arguments
 let getScoreText = function(name = 'Anonymous', score = 0) {
-    // console.log(name)
-    // console.log(score)
-    return 'Name: ' + name + ' - Score: ' + score
+    return `Name: ${name} - Score: ${score}` // Name: Anonymous - Score: 0
 }
 
 getScoreText() //  Anonymous 0
@@ -19,8 +17,14 @@ console.log(scoreText) // Name: Anonymous - Score: 0
 
 
 let getTip = function(total, tipPercent = .2) {
-    return total * tipPercent
+    let percent = tipPercent * 100
+    let tip = total * tipPercent
+    return `A ${percent}% tip on $${total} would be $${tip}.` // A 20% tip on $100 would be $20.
 }
 
 let tip = getTip(100)
 console.log(tip) // 20
+
+let name = 'Jen'
+let age = 12
+console.log(`Hey, my name is ${name}! I am ${age} years old.`) // Hey, my name is Jen! I am 12 years old
