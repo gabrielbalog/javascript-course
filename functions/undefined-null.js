73587bd1cc442bd78b6a6321140a3a838f21f564
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-02 23:18:57 -0300

    4 - 3. Undefined and Null

---
+++ functions/undefined-null.js
@@ -0,0 +1,28 @@
+// Undefined for variables
+let name // undefined
+
+name = 'Jen'
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+console.log(name)
+
+// Undefined for function arguments
+// Undefined as function return default value
+let square = function(num) {
+    console.log(num) // undefined
+}
+
+let result = square()
+console.log(result) // undefined
+
+
+// Null as assigned value
+let age = 27
+age = null
+
+console.log(age) // undefined -> null
