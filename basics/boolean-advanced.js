20037bfb34ab90eb69690d449b910cc731ba5203
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-02 22:40:27 -0300

    3 - 8. Advanced If Statements

---
+++ basics/boolean-advanced.js
@@ -0,0 +1,20 @@
+let isAccountLocked = true
+let userRole = 'admin'
+
+if (isAccountLocked) {
+    console.log('Is account locked')
+} else if (userRole === 'admin') {
+    console.log('Welcome Admin')
+} else {
+    console.log('Welcome!')
+}
+
+let temp = 45
+
+if (temp <= 32) {
+    console.log('It is freezing outside!')
+} else if (temp => 110) {
+    console.log('It is way to hot outside!')
+} else {
+    console.log('It is pretty nice.')
+}
