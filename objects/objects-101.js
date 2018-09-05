3d7ab9c48340be66282a33da3ae265e5ce7c6530
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-05 18:02:50 -0300

    5 - 2. Object Basics

---
+++ objects/objects-101.js
@@ -0,0 +1,19 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+let Person = {
+    name: 'Gabriel Balog',
+    age: 21,
+    location: 'São Paulo'
+}
+
+console.log(`${Person.name} is ${Person.age} and lives in ${Person.location}`)
