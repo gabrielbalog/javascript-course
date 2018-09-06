13a1ef4700bdae3796d325c79056d4fa07dd2e5e
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-06 18:19:10 -0300

    6 - 2. Array Basics

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ['Lavar louca', 'Lavar roupa', 'Dobrar a roupa', 'Passar pano', 'Molhar o cacto']
+
+console.log(`You have ${todos.length} todos!`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[2]}`)
