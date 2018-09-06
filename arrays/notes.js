024a1470ae7d5b16e518e72711298a2ba74e1298
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-06 18:28:53 -0300

    6 - 3. Manipulation Arrays with Methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,5 +1,17 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+// console.log(notes.pop()) // Note 3 -> Remove o ultimo item
+// notes.push('My new note') // append
+
+// console.log(notes.shift()) // Remove o primeiro item
+// console.log(notes.unshift('My first note')) // Adiciona no comeco
+
+// notes.splice(1, 2) // Remove 2 itens a partir da posicao 1
+// notes.splice(1, 0, 'This is the new second item') // Adiciona a string na segunda posicao 
+// notes.splice(1, 1, 'This is the new second item') // Atualiza o valor da segunda posicao com a string
+
+notes[1] = 'This is the new second item' // Atualiza o segundo item com a string
+
 console.log(notes)
 console.log(notes.length)
 console.log(notes[0])
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,8 @@
 const todos = ['Lavar louca', 'Lavar roupa', 'Dobrar a roupa', 'Passar pano', 'Molhar o cacto']
 
+todos.splice(2,1) // remove 3 item
+todos.push('Guardar a louca') // adiciona ao final do array
+todos.shift() // remove primeiro item
+
 console.log(`You have ${todos.length} todos!`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[2]}`)
+console.log(todos)
