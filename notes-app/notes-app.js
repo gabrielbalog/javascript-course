41c64ede690c058f672aecbbee10208c3836c0ea
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-08 00:27:30 -0300

    7 - 6. Adding Elements via the DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,13 @@
-// DOM
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 // const p = document.querySelector('p') // Match the first element
 // p.remove()
@@ -10,3 +19,8 @@ ps.forEach(function(p) {
     // console.log(p.textContent)
     // p.remove()
 })
+
+// Add new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from Javascript'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,42 @@
-ps = document.querySelectorAll('p')
+const todos = [{
+    text: 'Lavar louca',
+    completed: false
+},{
+    text: 'Lavar roupa',
+    completed: true
+},{
+    text: 'Dobrar a roupa',
+    completed: true
+},{
+    text: 'Passar pano',
+    completed: false
+},{
+    text: 'Molhar o cacto',
+    completed: false
+}]
 
-ps.forEach(function(p) {
-    if (p.textContent.includes('the')) {
-        p.remove()
-    }
+// let todosLeft = 0
+// todos.forEach(function(todo) {
+//     if (!todo.completed) {
+//         todosLeft++
+//     }
+
+// })
+
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = `You have ${todosLeft} todos left`
+// document.querySelector('body').appendChild(newParagraph)
+
+const incompleteTodos = todos.filter(function(todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
 })
