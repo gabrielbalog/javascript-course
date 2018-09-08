ad7a21d73f80643adc2e7172957a8ff25ab3b3ec
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-08 00:01:22 -0300

    7 - 4. DOM Manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,12 @@
-console.log('This is my from different file')
+// DOM
+
+// const p = document.querySelector('p') // Match the first element
+// p.remove()
+
+const ps = document.querySelectorAll('p') // match list all the elements
+
+ps.forEach(function(p) {
+    p.textContent = '***************'
+    // console.log(p.textContent)
+    // p.remove()
+})
