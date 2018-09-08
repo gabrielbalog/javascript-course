321d0dd8322e2408e2511e4608f17e1e0ef80709
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-07 22:16:10 -0300

    6 - 5. The For Loop

--- arrays/notes.js
+++ arrays/notes.js
@@ -20,3 +20,11 @@ notes.forEach(function(item, index) {
 console.log(notes)
 console.log(notes.length)
 console.log(notes[0])
+
+for (let count = 0; count <= 2; count++) {
+    console.log(count)
+}
+
+for (let count = notes.length - 1; count >= 0; count--) {
+    console.log(notes[count])
+}
