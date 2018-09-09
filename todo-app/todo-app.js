e2b2c2a58324d8f78e73f361f13dc98e82637ab1
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-08 22:23:40 -0300

    7 - 11. Todo Filter Challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,32 +15,34 @@ const todos = [{
     completed: false
 }]
 
-// let todosLeft = 0
-// todos.forEach(function(todo) {
-//     if (!todo.completed) {
-//         todosLeft++
-//     }
+const filters = {
+    searchText: '',
+}
 
-// })
 
-// const newParagraph = document.createElement('p')
-// newParagraph.textContent = `You have ${todosLeft} todos left`
-// document.querySelector('body').appendChild(newParagraph)
+renderTodos = function(todos, filters) {
+    const todosFiltered = todos.filter(function(todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-const incompleteTodos = todos.filter(function(todo) {
-    return !todo.completed
-})
+    document.querySelector('#todos').innerHTML = ''
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+    const incompleteTodos = todosFiltered.filter(function(todo) {
+        return !todo.completed
+    })
 
-todos.forEach(function(todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+    
+    todosFiltered.forEach(function(todo) {
+        const todoEl = document.createElement('p')
+        todoEl.textContent = todo.text
+        document.querySelector('#todos').appendChild(todoEl)
+    })
+}
 
+renderTodos(todos, filters)
 
 document.querySelector('#add-todo').addEventListener('click', function(e) {
     console.log('Add a new todo...')
@@ -48,4 +50,9 @@ document.querySelector('#add-todo').addEventListener('click', function(e) {
 
 document.querySelector('#new-todo-text').addEventListener('input', function(e) {
     console.log(e.target.value)
+})
+
+document.querySelector('#search-todo-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
