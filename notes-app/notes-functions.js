7b49eb3908d5b5f3b5c86130e6ea79b632bf626d
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-08 23:31:59 -0300

    8 - 4. Splitting up Our Application Code

---
+++ notes-app/notes-functions.js
@@ -0,0 +1,42 @@
+// Read existing notes from LocalStorage
+const getSavedNotes = function() {
+    const notesJSON = localStorage.getItem('notes')
+    
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
+
+// Save the notes to LocalStorage
+const saveNotes = function(note) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Generate the DOM structure for a note
+const generateNoteDom = function(note) {
+    const noteEl = document.createElement('p')
+
+    if (note.title.length > 0) {
+        noteEL.textContent = note.title
+    } else {
+        noteEl.textContent = 'Unnamed note'
+    }
+
+    return noteEl
+}
+
+// Render Application notes
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function(note) {
+        const noteEl = generateNoteDom(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
