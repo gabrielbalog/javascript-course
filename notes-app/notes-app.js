01c771ec36781ba340d894e8ce703387d1268c76
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-09 21:22:40 -0300

    8 - 14. JavaScript Dates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -32,3 +32,32 @@ window.addEventListener('storage', function(e) {
         renderNotes(notes, filters)
     }
 })
+
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+// const now = new Date('January 1 2018 00:00:00')
+// const now = new Date()
+// console.log(now.getTime())
+// console.log(now.toString())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of Month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minutes: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+const dateOne = new Date('March 1 2018 12:00:00')
+const dateTwo = new Date('June 1 2018 12:00:00')
+const dateOneTimestamp = dateOne.getTime()
+const dateTwoTimestamp = dateTwo.getTime()
+
+if (dateOneTimestamp < dateTwoTimestamp) {
+    console.log(dateOne.toString())
+} else if (dateTwoTimestamp < dateOneTimestamp) {
+    console.log(dateTwo.toString())
+}
