373d62e4532f7b9da30744d8fa7cd7650f01ba65
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-11 18:52:15 -0300

    9 - 6. Type Coercion

---
+++ advanced/type-coercion.js
@@ -0,0 +1,9 @@
+// Type coercion - a string, a number, a boolean
+
+// console.log('5' + 5)
+// console.log('5' - 5)
+// console.log('5' === 5)
+const value = true + 12
+const type = typeof value
+console.log(value)
+console.log(type)
