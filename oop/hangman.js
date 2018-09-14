f5f9536fb04db4f2c5ed22ee5df128532f3a5406
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-14 20:26:05 -0300

    10 - 5. Hangman Challenge Part I

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,26 @@
 const Hangman = function(word, numberOfGuesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.numberOfGuesses = numberOfGuesses
+    this.guessedLetters = ['c','a']
 }
 
+Hangman.prototype.getPuzzle = function() {
+    let puzzle = ''
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
+}
+
+
 const game1 = new Hangman('cat',2)
-console.log(game1)
+console.log(game1.getPuzzle())
 
 const game2 = new Hangman('California', 10)
-console.log(game2)
+console.log(game2.getPuzzle())
