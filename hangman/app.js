2c6ae0eeb120fb242b69683cf72505af484ddcb1
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-16 14:34:29 -0300

    11 - 2. HTTP Requests from JavaScript

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,6 @@
+// HTTP - (Hypertext transfer protocol)
+// Request - What do we want to do
+// Response - What was actually done
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 
@@ -11,4 +14,15 @@ window.addEventListener('keypress', function    (e) {
     guessesEl.textContent = game1.statusMessage
 })
 
-console.log(game1)
+// Making a HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
