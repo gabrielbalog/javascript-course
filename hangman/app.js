9bfabe3961a58b6482d770854f66ac4858761beb
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-16 15:25:34 -0300

    11 - 4. Exploring Another API

--- hangman/app.js
+++ hangman/app.js
@@ -28,3 +28,20 @@ request.addEventListener('readystatechange', (e) => {
 
 request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
+
+const countryCode = 'BR'
+const countryRequest = new XMLHttpRequest()
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const myCountry = data.find((country) => {
+            return country.alpha2Code === countryCode
+        })
+        console.log(myCountry.name)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
+    }
+})
+
+countryRequest.open('GET','https://restcountries.eu/rest/v2/all')
+countryRequest.send()
