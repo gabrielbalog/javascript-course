0094025b500b81ff728812861aa45216c7ae77ba
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-16 16:32:34 -0300

    11 - 7. Callback Abstraction Challenge

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,3 @@
-// HTTP - (Hypertext transfer protocol)
-// Request - What do we want to do
-// Response - What was actually done
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 
@@ -14,7 +11,7 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle((error, puzzle) => {
+getPuzzle("1",(error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
@@ -22,22 +19,10 @@ getPuzzle((error, puzzle) => {
     }
 })
 
-// Making a HTTP request
-
-
-// const countryCode = 'BR'
-// const countryRequest = new XMLHttpRequest()
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const myCountry = data.find((country) => {
-//             return country.alpha2Code === countryCode
-//         })
-//         console.log(myCountry.name)
-//     } else if (e.target.readyState === 4) {
-//         console.log('An error has taken place')
-//     }
-// })
-
-// countryRequest.open('GET','https://restcountries.eu/rest/v2/all')
-// countryRequest.send()
+getCountry("LT", (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(country)
+    }
+})
