d5039e0e98bdd13e13c932ce388962dcc20ea9f2
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-17 14:07:07 -0300

    13 - 11. Webpack Dev Server

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -2,7 +2,7 @@ import otherSquare, { add, name } from './utilities'
 import scream from './scream'
 
 console.log('index.js')
-console.log(add(32,1))
+console.log(add(35,1))
 console.log(name)
 console.log(scream(name))
 console.log(otherSquare(3))
