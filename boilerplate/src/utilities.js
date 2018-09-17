23fb597665dee7dc74973d8f63023b0e08ef1fde
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-17 13:45:15 -0300

    13 - 9. JavaScript Modules Part II

--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,5 +1,10 @@
-console.log('utilities.js')
+// Named export
+// Default export
 
-export const add = (a,b) => a + b
+const add = (a,b) => a + b
 
-export const name = 'Gabriel'
+const name = 'Gabriel'
+
+const square = (x) => x * x
+
+export { add, name, square as default}
