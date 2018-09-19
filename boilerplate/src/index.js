0c62869068e3260b9f9def5145ca8d0a0e771c9a
Gabriel Balog <gabriel.balog@bradootech.com>
2018-09-19 14:11:12 -0300

    13 - 23. The Spread Syntax

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,16 +1,17 @@
-const calculateAverage = (thing, numbers) => {
-    let sum = 0
-    numbers.forEach((num) => sum += num)
-    const average = sum / numbers.length
-    return `The average ${thing} is ${average}`
-}
-
-console.log(calculateAverage('age', 0, 100, 68, 14))
-
 const printTeam = (teamName, coach, ...players) => {
     console.log(`Team: ${teamName}`)
     console.log(`Coach: ${coach}`)
     console.log(`Players: ${players.join(', ')}`)
 }
 
-console.log(printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry'))
+const team = {
+    name: 'Liberty',
+    coach: 'Casey Penn',
+    players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+}
+
+printTeam(team.name, team.coach, ...team.players)
+
+let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
+cities = [...cites, 'Santiago']
+console.log(cities)
--- hangman/src/hangman.js
+++ hangman/src/hangman.js
@@ -45,7 +45,7 @@ class Hangman {
     
         if (this.status === 'playing') {
             if (isUnique) {
-                this.guessedLetters.push(guess)
+                this.guessedLetters = [...this.guessedLetters, guess]
             }
         
             if (isUnique && isBadGuess) {
